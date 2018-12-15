4e98b8a2b9212df7cedf75f6567d074603cadcf1
Takumi Ito <ito.tkmi@gmail.com>
2018-12-16 04:00:09 +0800

    Downgrade typescript version

--- src/app/static-page/static-page.module.ts
+++ src/app/static-page/static-page.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { StaticPageRoutingModule } from './static-page-routing.module';
 
 import { SharedModule } from '../shared/shared.module';
-import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
   imports: [
     StaticPageRoutingModule,
     SharedModule,
-    NgxsModule.forFeature([])
   ],
   declarations: []
 })
